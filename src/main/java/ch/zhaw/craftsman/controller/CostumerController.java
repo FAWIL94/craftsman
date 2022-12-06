4f060dfdbb19fa941ff4f58929b17c9a0626d0a6
wildfab1 <wildfab1@students.zhaw.ch>
2022-12-06 10:43:47 +0100

    add endpoint to get all Customers

--- src/main/java/ch/zhaw/craftsman/controller/CostumerController.java
+++ src/main/java/ch/zhaw/craftsman/controller/CostumerController.java
@@ -1,8 +1,11 @@
 package ch.zhaw.craftsman.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping; 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,4 +27,9 @@ public class CostumerController {
             Customer c = customerRepository.save(fDAO); 
             return new ResponseEntity<>(c, HttpStatus.CREATED); 
         }
+    @GetMapping("/customer")
+    public ResponseEntity<List<Customer>> getAllCustomers(){
+        List<Customer> allCustomer = customerRepository.findAll();
+        return new ResponseEntity<>(allCustomer, HttpStatus.OK);
+    }
 }
