0c20b88be38dc24d5d822a9566eeb587f553ba36
wildfab1 <wildfab1@students.zhaw.ch>
2022-12-06 10:18:33 +0100

    first commit - add mongodb

---
+++ src/test/java/ch/zhaw/craftsman/CraftsmanApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.craftsman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftsmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
